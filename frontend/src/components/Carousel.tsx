7bed81e00484f8af0f61c1fcf35818921896b8a0
Egor1o <egorklo147@gmail.com>
2024-10-04 21:34:05 +0300

    Astro React Tailwind

---
+++ frontend/src/components/Carousel.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Carousel: React.FC = () => {
+    return (
+        <div>
+            <h1 className='text-red-600'>
+                Hello brothers and sisters.
+            </h1>
+        </div>
+    )
+}
+
+export default Carousel
