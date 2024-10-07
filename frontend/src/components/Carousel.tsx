de73657e8c9d80e93b439b630624e52146085be6
Egor1o <egorklo147@gmail.com>
2024-10-07 23:30:51 +0300

    Base for carousel. No strapi yet. Responsiveness is lagging.

--- frontend/src/components/Carousel.tsx
+++ frontend/src/components/Carousel.tsx
@@ -1,11 +1,82 @@
-import React from 'react';
+import React, { useState, useRef, useEffect } from 'react';
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+import PersonCard from './PersonCard.tsx';
 
-const Carousel: React.FC = () => {
+const responsive = {
+  desktop: {
+    breakpoint: { max: 3000, min: 2100 },
+    items: 5,
+    slidesToSlide: 1 // optional, default to 1.
+  },
+  tablet: {
+    breakpoint: { max: 2099, min: 768 },
+    items: 3,
+    slidesToSlide: 1 // optional, default to 1.
+  },
+  mobile: {
+    breakpoint: { max: 767, min: 464 },
+    items: 2,
+    slidesToSlide: 1 // optional, default to 1.
+  }
+};
+
+const sliderImageUrl = [
+  { url: '../../public/8240494.jpg' },
+  { url: '../../public/8240494.jpg' },
+  { url: '../../public/8240494.jpg' },
+  { url: '../../public/8240494.jpg' },
+  { url: '../../public/8240494.jpg' }
+];
+
+const CarouselComponent: React.FC = () => {
+  const [carouselWidth, setCarouselWidth] = useState<number>(0); // Store the width of the carousel
+  const carouselRef = useRef<HTMLDivElement>(null); // Ref for the carousel container
+  const [currentSlide, setCurrentSlide] = useState<number>()
+  const [currentFocus, setCurrentFocus] = useState<number>()
+
+  useEffect(() => {
+    // Measure the width of the carousel when the component is mounted
+    if (carouselRef.current) {
+      setCarouselWidth(carouselRef.current.offsetWidth);
+    }
+
+
+    const handleResize = () => {
+      if (carouselRef.current) {
+
+        setCarouselWidth(carouselRef.current.offsetWidth);
+      }
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
   return (
-    <div>
-      <h1 className='text-red-600'>Hello brothers and sisters.</h1>
+    <div className="parent bg-[#EC4D4D] bg-opacity-75" ref={carouselRef}>
+      <p>Carousel width: {carouselWidth}px</p> {/* Display the carousel width */}
+      <Carousel
+        responsive={responsive}
+        swipeable={true}
+        draggable={true}
+        showDots={true}
+        infinite={true}
+        partialVisible={false}
+        dotListClass="custom-dot-list-style"
+        afterChange={(previousSlide,{currentSlide}) =>{
+
+        }}
+      >
+        {sliderImageUrl.map((imageUrl, index) => (
+          <div className='mb-10 mt-10' key={index}>
+            <PersonCard name={`Person ${index + 1}`} />
+          </div>
+        ))}
+      </Carousel>
     </div>
   );
 };
 
-export default Carousel;
+export default CarouselComponent;
