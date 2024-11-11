972d6483cd808195935a8514329bd6a437669d13
tuomax7 <tuomas.nummela.per@gmail.com>
2024-11-11 23:14:03 +0200

    feat: add first version of ig feed

---
+++ frontend/src/components/InstagramFeed.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const InstagramFeed: React.FC = () => (
+  <div>
+    <script src='https://static.elfsight.com/platform/platform.js' async></script>
+    <div className='elfsight-app-f77650ca-df40-4bfe-92ec-6cc0cebd267e' data-elfsight-app-lazy></div>
+  </div>
+);
+
+export default InstagramFeed;
